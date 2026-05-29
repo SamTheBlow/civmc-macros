bb88be1a99c088719750a7f1e60d1b05c6403c0c
SamTheBlow <pokeyoshi@hotmail.com>
2026-05-29 19:39:03 -0400

    Improve and optimize vine bot

--- vine.js
+++ vine.js
@@ -1,4 +1,6 @@
-// Bot to use to passively farm vines.
+// Bot to passively farm vines.
+// The farm is expected to go from east to west, starting on the east side.
+// You'll need plenty of shears, and a small amount of food.
 
 // Feel free to change the key to manually stop the bot.
 const abortKey = "tab";
@@ -15,8 +17,8 @@ const startCoords = [-3424, -3317];
 // Put here the coords of the block where the bot will turn around (only x and z).
 const endCoords = [-3585, -3317];
 
-// Put here the number of floors your farm has.
-const numberOfFloors = 2;
+// Put here the y level of each floor your farm has, from lowest to highest y level.
+const floors = [-53, -43];
 
 // Will try to refill on shears if you don't have this many on you.
 // Might want to make it higher if your farm is longer and/or has more floors.
@@ -37,15 +39,18 @@ const shearsChests = [
 
 const BOT_NAME = "VineBot";
 const ABORT_MANUALLY = "Player has pressed the abort key.";
+const SHEARS = "minecraft:shears";
+const numberOfFloors = floors.length;
 
 p = Player.getPlayer();
+
+currentY = p.getPos().y;
 
 // Floor 1 is the lowest floor, then floor 2 is higher up, etc.
 currentFloor = 1;
 
-currentY = p.getPos().y;
-
-botMode = "mainSouth";
+botMode = "main";
+botFacingSouth = p.getYaw() > -90 && p.getYaw() < 90;
 
 // Note: the hotbar consists of slots 36 to 44.
 mainSlot = 36;
@@ -56,7 +61,12 @@ recalibrateTimer = 0;
 // This is needed for the disconnect code...
 isDisconnected = false;
 
-globalCounter = 0;
+// This is needed to make the bot stop for one tick on every block
+// This is to optimize bot yield
+lastStopX = Math.floor(p.getPos().x);
+// This is needed to make the bot walk forward a little bit on the glass pane
+// This is to optimize bot yield
+walkForward = false;
 
 // This makes you automatically log back in when you get disconnected
 const ticklistener_1 = JsMacros.on(
@@ -105,49 +115,80 @@ const ticklistener_1 = JsMacros.on(
   }),
 );
 
-// Ensure the user starts on the starting point
-if (!isOnBlock(startCoords[0], startCoords[1])) {
+// Ensure the user is on the farm
+if (
+  p.getPos().x > startCoords[0] + 1 ||
+  p.getPos().x < endCoords[0] ||
+  p.getPos().z > startCoords[1] + 1 ||
+  p.getPos().z < startCoords[1]
+) {
   botMode = "terminate";
-  terminateReason = "not standing on the starting block";
+  terminateReason = "not standing inside the farm";
 } else {
+  currentFloor = getCurrentFloor();
   goAfk();
-
-  // Refill on shears at the very start
-  checkRefillShears();
 }
 
 mainLoop();
 
 // Main loop
 function mainLoop() {
-  while (botMode != "terminate") {
-    checkFell();
+  // In case you start on a lodestone,
+  // ensure you start on the correct floor
+  if (botMode != "terminate" && standingOnLodestone()) {
+    KeyBind.keyBind("key.sneak", true);
+    Client.waitTick(5);
+    goToCurrentFloor();
+    if (currentFloor == 1 && isOnBlock(startCoords[0], startCoords[1])) {
+      checkRefillShears();
+    }
+  }
 
+  while (botMode != "terminate") {
     KeyBind.keyBind("key.sneak", true);
     grabShears();
     face();
 
     if (botMode == "recalibrateZ") {
+      KeyBind.keyBind("key.forward", false);
       KeyBind.keyBind("key.right", false);
       KeyBind.keyBind("key.attack", false);
       recalibrateZ();
-      recalibrateTimer++;
     }
-    if (botMode != "recalibrateZ") // Doing the check again is important here
+    if (botMode == "main") // Doing the check again is important here
     {
-      KeyBind.keyBind("key.right", globalCounter % 10 != 0);
+      // Makes the player move forward a little bit to ensure they always
+      // target a block (the wall if not vines).
+      // When there's nothing to hit, there's a cooldown before
+      // the bot can attack again and that is UNOPTIMAL!
+      KeyBind.keyBind(
+        "key.forward",
+        (p.getPos().z -
+          (startCoords[1] + 0.5 + (botFacingSouth ? 0.1 : -0.1))) *
+          (botFacingSouth ? 1 : -1) <
+          0,
+      );
+
+      // Makes the player stop moving right for one tick, on each block passed
+      // (this optimizes vines yield)
+      if (
+        (p.getPos().x - (lastStopX + (botFacingSouth ? -0.5 : 1.5))) *
+          (botFacingSouth ? 1 : -1) <
+        0
+      ) {
+        KeyBind.keyBind("key.right", false);
+        lastStopX = Math.floor(p.getPos().x);
+      } else {
+        KeyBind.keyBind("key.right", true);
+      }
+
       KeyBind.keyBind("key.attack", true);
-      globalCounter++;
-      checkStatus();
+
+      checkEndOfRow();
     }
 
-    // Failsafe to terminate if recalibrating fails
-    if (recalibrateTimer >= 10) {
-      botLog("Failed to recalibrate.");
-      terminateReason = "Bot is stuck somewhere not on the track";
-      botMode = "terminate";
-    }
-
+    checkWrongZ();
+    checkFell();
     checkManualAbort();
     Client.waitTick();
   }
@@ -189,10 +230,11 @@ function checkFell() {
   if (p.getPos().y < currentY - 0.5) {
     terminateReason = "Bot fell";
     botMode = "terminate";
-    return;
   }
+}
 
-  // Failsafe: make sure the bot stays on the same z coordinate
+// This is a failsafe to make sure the bot stays on the correct z coordinate
+function checkWrongZ() {
   if (botMode != "recalibrateZ" && !isOnTrack()) {
     recalibrateTimer = 0;
     botLog("Failsafe: recalibrating Z position...");
@@ -208,58 +250,29 @@ function checkManualAbort() {
   }
 }
 
-// Stuff that happens when you're done doing a row
-function checkStatus() {
+function checkEndOfRow() {
   // Check if we're at the end of the farm
-  if (botMode == "mainSouth" && isOnBlock(endCoords[0], endCoords[1])) {
+  if (botFacingSouth && isOnBlock(endCoords[0], endCoords[1])) {
     // Turn around and do the other side
-    botMode = "mainNorth";
-    face();
+    botFacingSouth = false;
     return;
   }
 
   // Check if we're back at the start of the farm
-  if (botMode == "mainNorth" && isOnBlock(startCoords[0], startCoords[1])) {
+  if (!botFacingSouth && isOnBlock(startCoords[0], startCoords[1])) {
+    KeyBind.keyBind("key.right", false);
+    KeyBind.keyBind("key.attack", false);
+
     if (currentFloor < numberOfFloors) {
       // We haven't reached the top floor yet.
       // Go to next floor.
-      yBefore = p.getPos().y;
-      KeyBind.keyBind("key.jump", true);
-      Client.waitTick(5);
-      KeyBind.keyBind("key.jump", false);
-      yAfter = p.getPos().y;
-      if (yBefore < yAfter) {
-        // We made it to the next floor.
-        currentFloor++;
-        currentY = p.getPos().y;
-      } else {
-        // We didn't make it to the next floor.
-        terminateReason = "Failed to go up the elevator";
-        botMode = "terminate";
-        return;
-      }
+      currentFloor++;
+      goToCurrentFloor();
     } else {
       // We are on the topmost floor.
       // Return all the way to floor 1 and start over from floor 1.
-      while (currentFloor > 1) {
-        KeyBind.keyBind("key.right", false);
-        yBefore = p.getPos().y;
-        KeyBind.keyBind("key.sneak", false);
-        Client.waitTick(10);
-        KeyBind.keyBind("key.sneak", true);
-        Client.waitTick(10);
-        yAfter = p.getPos().y;
-        if (yBefore > yAfter) {
-          // We made it to the previous floor.
-          currentFloor--;
-        } else {
-          // We didn't make it to the previous floor.
-          terminateReason = "Failed to go down the elevator";
-          botMode = "terminate";
-          return;
-        }
-      }
-      currentY = p.getPos().y;
+      currentFloor = 1;
+      goToCurrentFloor();
 
       // Make sure there's always free space in the inventory
       // Throw away all the vines that are in the inventory
@@ -268,17 +281,13 @@ function checkStatus() {
       checkRefillShears();
     }
 
-    // Turn around
-    botMode = "mainSouth";
-    face();
-
-    // Eat if needed
+    botFacingSouth = true;
     eatCheck();
   }
 }
 
 function grabShears() {
-  grabItem(["minecraft:shears"], "shears");
+  grabItem([SHEARS], "shears");
 }
 
 function grabFood() {
@@ -340,21 +349,14 @@ function eatCheck() {
   KeyBind.keyBind("key.attack", false);
   botLog("Food level low, auto eating");
   grabFood();
-  Client.waitTick(10);
-  KeyBind.key("key.mouse.right", true);
-  Client.waitTick(33);
-  KeyBind.key("key.mouse.right", false);
-  KeyBind.keyBind("key.right", true);
-  KeyBind.keyBind("key.attack", true);
-  grabShears();
+  Client.waitTick(5);
+  KeyBind.keyBind("key.use", true);
+  Client.waitTick(35);
+  KeyBind.keyBind("key.use", false);
 }
 
 function face() {
-  angle = 0;
-  if (botMode == "mainNorth") {
-    angle = 180;
-  }
-  p.lookAt(angle, 0);
+  p.lookAt(botFacingSouth ? 0 : 180, 0);
   Client.waitTick();
 }
 
@@ -391,36 +393,31 @@ function numberInInventory(itemToCount) {
   inv = Player.openInventory();
   number = inv.findItem(itemToCount).length;
   inv.close();
+  Client.waitTick();
   return number;
 }
 
 // Checks if we need to refill on shears. If so, does it
 function checkRefillShears() {
+  numberOfShears = numberInInventory(SHEARS);
+  if (numberOfShears >= minimumShears) {
+    return;
+  }
+
   KeyBind.keyBind("key.right", false);
   KeyBind.keyBind("key.attack", false);
   KeyBind.keyBind("key.sneak", false);
 
   // Wait for the player to unsneak
   // Otherwise they might not look at the right chest
-  Time.sleep(500);
-
-  numberOfShears = numberInInventory("minecraft:shears");
-
-  if (numberOfShears >= minimumShears) {
-    return;
-  }
+  Client.waitTick(5);
 
   // Grab shears from nearby chests until we have enough
   for (i = 0; i < shearsChests.length; i++) {
-    // Look at chest
-    lookAtCenter(shearsChests[i][0], shearsChests[i][1], shearsChests[i][2]);
-    Client.waitTick();
-    // Open chest
-    p.interact();
-    Client.waitTick(5);
+    openChest(shearsChests[i]);
     // Take shears until inventory is full or chest is empty
     inv = Player.openInventory();
-    shearsSlots = inv.findItem("minecraft:shears");
+    shearsSlots = inv.findItem(SHEARS);
     containerSlots = inv.getSlots("container");
     for (shearsSlot of shearsSlots) {
       if (!(shearsSlot in containerSlots)) {
@@ -431,7 +428,7 @@ function checkRefillShears() {
     }
     inv.close();
     Client.waitTick();
-    numberOfShears = numberInInventory("minecraft:shears");
+    numberOfShears = numberInInventory(SHEARS);
     if (numberOfShears >= minimumShears) {
       break;
     }
@@ -441,16 +438,21 @@ function checkRefillShears() {
 // Throws away all vines from the inventory.
 // Expects you to be standing on the starting block.
 function throwAwayVines() {
-  // Face towards the water collection
-  p.lookAt(60, 0);
-  Client.waitTick();
-
   inv = Player.openInventory();
+
   vinesSlots = inv.findItem("minecraft:vine");
+
+  if (vinesSlots.length > 0) {
+    // Face towards the water collection
+    p.lookAt(60, 0);
+    Client.waitTick();
+  }
+
   for (vinesSlot of vinesSlots) {
     inv.dropSlot(vinesSlot, true);
     Client.waitTick();
   }
+
   inv.close();
   Client.waitTick();
 }
@@ -458,19 +460,104 @@ function throwAwayVines() {
 // Failsafe for when the z coordinate is wrong.
 // Attempts to get back on the track by moving forwards/backwards.
 function recalibrateZ() {
-  if (p.getPos().z < startCoords[1] + 0.5) {
-    KeyBind.keyBind("key.forward", true);
-    KeyBind.keyBind("key.back", false);
-  } else {
-    KeyBind.keyBind("key.forward", false);
-    KeyBind.keyBind("key.back", true);
-  }
+  goForward =
+    (p.getPos().z - (startCoords[1] + 0.5)) * (botFacingSouth ? 1 : -1) < 0;
+  KeyBind.keyBind("key.forward", goForward);
+  KeyBind.keyBind("key.back", !goForward);
 
   // Go back to normal once we're back on track
   if (isOnTrack()) {
     botLog("Finished recalibrating Z position, we're all good now");
     KeyBind.keyBind("key.forward", false);
     KeyBind.keyBind("key.back", false);
-    botMode = "mainSouth";
+    botMode = "main";
+    return;
+  }
+
+  // Failsafe: terminate if recalibrating fails
+  recalibrateTimer++;
+  if (recalibrateTimer >= 10) {
+    botLog("Failed to recalibrate.");
+    terminateReason = "Bot is stuck somewhere not on the track";
+    botMode = "terminate";
+  }
+}
+
+function getCurrentFloor() {
+  floorFound = floors.indexOf(currentY);
+  if (floorFound == -1) {
+    botLog(
+      "couldn't find what floor we're on. Player's y value is " + currentY,
+    );
+    return 1;
+  }
+  return floorFound + 1;
+}
+
+function standingOnLodestone() {
+  // We put the end coords here anyway
+  // even though we never use that lodestone just for the edge case
+  // where we reconnect while standing on the lodestone
+  return (
+    isOnBlock(startCoords[0], startCoords[1]) ||
+    isOnBlock(endCoords[0], endCoords[1])
+  );
+}
+
+function goToCurrentFloor() {
+  targetY = floors[currentFloor - 1];
+  currentY = p.getPos().y;
+
+  if (currentY == targetY) {
+    botLog("already on the correct floor!");
+  } else if (currentY < targetY) {
+    // Go up to next floor
+    while (currentY < targetY) {
+      botLog("going up a floor...");
+      yBefore = currentY;
+      KeyBind.keyBind("key.jump", true);
+      Client.waitTick(5);
+      KeyBind.keyBind("key.jump", false);
+      // Wait a bit for the teleportation to finish
+      Client.waitTick(5);
+      currentY = p.getPos().y;
+    }
+    botLog("went up to the correct floor!");
+  } else {
+    // Go down to previous floor
+    while (currentY > targetY) {
+      botLog("going down a floor...");
+      KeyBind.keyBind("key.right", false);
+      yBefore = currentY;
+      KeyBind.keyBind("key.sneak", false);
+      Client.waitTick(5);
+      KeyBind.keyBind("key.sneak", true);
+      Client.waitTick(5);
+      currentY = p.getPos().y;
+    }
+    botLog("went down to the correct floor!");
+  }
+}
+
+// chestCoords should be an array with 3 elements, x y z
+function openChest(chestCoords, attempts = 0) {
+  // Prevent infinite loop
+  if (attempts >= 10) {
+    return;
+  }
+
+  lookAtCenter(chestCoords[0], chestCoords[1], chestCoords[2]);
+  KeyBind.keyBind("key.use", true);
+  Client.waitTick();
+  KeyBind.keyBind("key.use", false);
+
+  // Wait for the chest to open
+  Client.waitTick(5);
+  inv = Player.openInventory();
+  if (!inv || inv.getTotalSlots() != 90) {
+    inv.close();
+    Client.waitTick();
+    // Try again
+    openChest(chestCoords, attempts + 1);
   }
 }
